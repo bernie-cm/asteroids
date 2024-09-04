4ba3479181b02e2d09d31b17fa4b7712ab8839c5
bernie-cm <bernie.camejo@icloud.com>
2024-09-05 06:47:23 +1000

    add Clock object and set FPS rate

--- main.py
+++ main.py
@@ -6,8 +6,12 @@ def main():
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    # Create a pygame Clock object to track of time
+    clock = pygame.time.Clock()
+    delta = 0
+
     while True:
-        # his will check if the user has closed the window
+        # This will check if the user has closed the window
         # and exit the game loop if they do
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -16,6 +20,8 @@ def main():
         screen.fill("BLACK")    # Set the rectangle to be solid black
         pygame.display.flip()
 
+        # Limit the frame rate to 60 FPS
+        delta = clock.tick(60) / 1000  # Convert from msec to sec
 
 
 if __name__ == "__main__":
